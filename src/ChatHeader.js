ba9331e5766b1017505eb22774dac1587bd19136
Ajay S <ajay_sieunarine@hotmail.com>
2020-10-10 17:28:13 -0400

    wtf

---
+++ src/ChatHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Chat.css';
+import ChatHeader from "./ChatHeader";
+
+function Chat() {
+    return (
+        // bem naming convention (from Chat to Chat)
+        <div className="chat">
+
+        </div>
+        <div className="chat"> {/* main div */}
+            <ChatHeader />
+        </div> // main div
+    );
+}
+
