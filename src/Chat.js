a8f8e8f845607b64985e1d5fc372ab62bf93c6ef
Ajay S <ajay_sieunarine@hotmail.com>
2020-10-10 17:56:03 -0400

    wrap up chat component

--- src/Chat.js
+++ src/Chat.js
@@ -2,12 +2,35 @@ import React from 'react';
 import './Chat.css';
 import ChatHeader from "./ChatHeader";
 
+// icons
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+
 function Chat() {
     return (
         // bem naming convention (from Chat to Chat)
         <div className="chat"> {/* main div */}
             <ChatHeader />
-        </div>
+
+            <div className="chat__messages">
+            
+            </div>
+            <div className="chat__input">
+                <AddCircleIcon fontSize={"large"}/>
+                <form action="">
+                    <input type="text" placeholder={`Message #TESTCHANNEL`}/>
+                    <button className={"chat__inputButton"} type={"submit"}>Send Message</button>
+                </form>
+                <div className="chat__inputIcons">
+                    <CardGiftcardIcon fontSize={"large"}/>
+                    <GifIcon fontSize={"large"}/>
+                    <EmojiEmotionsIcon fontSize={"large"}/>
+                </div>
+            </div>
+
+        </div> // main div
     );
 }
 
