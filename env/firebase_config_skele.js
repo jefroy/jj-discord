94e60ba48c1a4591b274cbf8750fa3dc0593e3d4
Ajay S <ajay_sieunarine@hotmail.com>
2020-10-10 18:42:38 -0400

    add firebase to app

---
+++ env/firebase_config_skele.js
@@ -0,0 +1,8 @@
+// populate this from firebase app from firebase dashboard copy this file and remove the '_skele' part.
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+
+};
+
+// export {firebaseConfig};
