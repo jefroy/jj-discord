762b5ba389e56f49ece72eb8cee2f7478ad7ddec
Ajay S <ajay_sieunarine@hotmail.com>
2020-10-12 00:19:12 -0400

    add userslice and app slice (redux)

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -3,23 +3,22 @@ import { createSlice } from '@reduxjs/toolkit';
 export const appSlice = createSlice({
   name: 'app',
   initialState: {
-    app: null,
+    channelId: null,
+    channelName: null,
   },
   reducers: {
-    login: (state, action) => {
+    setChannelId: (state, action) => {
       state.app += action.payload;
-    },
-    logout: (state) => {
-      state.app = null;
     }
   },
 });
 
-export const { login, logout } = appSlice.actions;
+export const { setChannelId } = appSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = state => state.app.value;
+export const selectChannelId = state => state.app.channelId;
+export const selectChannelName = state => state.app.channelName;
 
 export default appSlice.reducer;
