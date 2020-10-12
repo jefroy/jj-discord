0d3bafe2fe3d779ff495c7379d243d1a451b9816
Ajay S <ajay_sieunarine@hotmail.com>
2020-10-12 00:28:40 -0400

    add user slice to app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from "./Chat";
+import {useSelector} from "react-redux";
+import {selectUser} from './features/userSlice';
 
 function App() {
+    const user = useSelector(selectUser);
   return (
     // bem naming convention (from App to app)
     <div className="app">
-      <Sidebar />
-      {/* chat */}
-      <Chat />
+        {user ? ( // check for login
+            // if logged in, render app
+            <>
+            <Sidebar />
+            <Chat />
+            </>
+        ) : (
+            // else, ask for login
+            <h2> Login plz </h2>
+        )}
     </div>
   );
 }
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -17,9 +17,6 @@ export const userSlice = createSlice({
 
 export const { login, logout } = userSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = state => state.user.value;
+export const selectUser = state => state.user.user;
 
 export default userSlice.reducer;
